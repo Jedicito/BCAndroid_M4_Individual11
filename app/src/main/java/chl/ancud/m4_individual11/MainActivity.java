dbfa6b3d9a419a8df06d8d5e99d5c2cdc1718e0e
Jedicito <seron.cesar@gmail.com>
2023-06-28 21:36:56 -0400

    Binding agregado. Botones muestran toast

--- app/src/main/java/chl/ancud/m4_individual11/MainActivity.java
+++ app/src/main/java/chl/ancud/m4_individual11/MainActivity.java
@@ -3,12 +3,70 @@ package chl.ancud.m4_individual11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import chl.ancud.m4_individual11.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnFlightMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarToast(getString(R.string.turn_off_flight_mode));
+            }
+        });
+
+        binding.btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarToast(getString(R.string.picture_saved));
+            }
+        });
+
+        binding.btnPictureSaved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarToast(getString(R.string.picture_saved));
+            }
+        });
+
+        binding.btnUnlockedLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarToast(getString(R.string.unlocked_level));
+            }
+        });
+
+        binding.btnLevelCompleted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarToast(getString(R.string.level_completed));
+            }
+        });
+
+        binding.btnMusicPaused.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarToast(getString(R.string.music_paused));
+            }
+        });
+
     }
+
+    protected void mostrarToast(String mensaje) {
+        Toast toast = Toast.makeText(this, mensaje, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+
 }
